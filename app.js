9ed02d378e3e487406186b1b14f63b7dad28136e
Bayardo <bjlslopez@gmail.com>
2021-10-22 10:28:11 -0600

    START NEW GAME is hidden when game starts

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
     // the game bars should be at 100
         // the healthbar width should be at 100%
         // the healthbar text should be at 100
+// when start new game is clicked
+    // the start new game button should be hidden
+    // the player control buttons should be shown
+// while no actions have been performed, the action log should be empty
 
 new Vue({
     el: "#app",
