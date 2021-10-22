aadc8bc52c9756feeacb3e6f0edf5b3871291a88
Bayardo <bjlslopez@gmail.com>
2021-10-22 08:46:52 -0600

    Health bar for YOU has been set at 100 for beginning the game

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 // while start new game button has not been clicked
     // the only button shown should be start new game
     // the game bars should be at 100
+        // the healthbar width should be at 100%
+        // the healthbar text should be at 100
 
 new Vue({
     el: "#app",
     data() {
         return {
-            gameStarted: false
+            gameStarted: false,
+            healthYOU: 100,
+            healthbarYOU: {
+                backgroundColor: "green",
+                margin: 0,
+                color: "white",
+                width: `100%`,
+            },
         }
     },
     methods: {
